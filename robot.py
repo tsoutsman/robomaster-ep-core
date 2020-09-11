21a6a72194f10ab292620a7685fafe6648c719fb
Klim Tsoutsman <klimusha@gmail.com>
2020-09-11 14:54:24 +1000

    Add connection and command mode

--- robot.py
+++ robot.py
@@ -1,17 +1,43 @@
 # TODO: add multithreading to enable robot to do stuff while streaming video
 
-import sockets
+import socket
 
 
 class Robot:
     def __init__(self):
-        pass
+        self.s = s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
-        pass
+        s.connect(("192.168.2.1", 40923))
+        print("Connected")
 
     def sendCommand(self, command):
-        pass
+        # sending command
+        self.s.send(command.encode("utf-8"))
 
-    def commandMode(self):
-        pass
+        # receiving command
+        try:
+            buf = self.s.recv(1024)
+            return buf.decode("utf-8")
+        except socket.error as e:
+            return f"Error receiving: {e}"
+
+    def commandMode(self, command):
+        # enabling command mode
+        sendCommand("command")
+        while True:
+            # quitting program
+            if command.lower() == "q":
+                break
+            elif command.lower() == "video stream":
+                enableVideoStream()
+            # correcting syntax
+            else:
+                if command[-1] != ";":
+                    command += ";"
+                response = sendCommand(command)
+                print(response)
+
+        # Disable the port connection
+        self.s.shutdown(socket.SHUT_WR)
+        self.s.close()
