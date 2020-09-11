848306442db69f4813ec0bd20424f397099df52f
Klim Tsoutsman <klimusha@gmail.com>
2020-09-11 14:28:23 +1000

    Initial commit

---
+++ robot.py
@@ -0,0 +1,17 @@
+# TODO: add multithreading to enable robot to do stuff while streaming video
+
+import sockets
+
+
+class Robot:
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def sendCommand(self, command):
+        pass
+
+    def commandMode(self):
+        pass
