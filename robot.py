1fa283aa49b9aa186910b472677cea5376189699
Klim Tsoutsman <klimusha@gmail.com>
2020-09-12 12:24:50 +1000

    Add basic video stream and bugfix

--- robot.py
+++ robot.py
@@ -1,36 +1,37 @@
 # TODO: add multithreading to enable robot to do stuff while streaming video
 
 import socket
+import cv2
 
 
 class Robot:
     def __init__(self):
-        self.s = s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.command_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.video_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
-        s.connect(("192.168.2.1", 40923))
-        print("Connected")
+        self.command_s.connect(("192.168.2.1", 40923))
 
     def sendCommand(self, command):
         # sending command
-        self.s.send(command.encode("utf-8"))
+        self.command_s.send(command.encode("utf-8"))
 
         # receiving command
         try:
-            buf = self.s.recv(1024)
+            buf = self.command_s.recv(1024)
             return buf.decode("utf-8")
         except socket.error as e:
             return f"Error receiving: {e}"
 
-    def commandMode(self, command):
+    def commandMode(self):
+        self.command_s.connect(("192.168.2.1", 40923))
         # enabling command mode
         sendCommand("command")
         while True:
+            command = input("Command: ")
             # quitting program
             if command.lower() == "q":
                 break
-            elif command.lower() == "video stream":
-                enableVideoStream()
             # correcting syntax
             else:
                 if command[-1] != ";":
@@ -39,5 +40,14 @@ class Robot:
                 print(response)
 
         # Disable the port connection
-        self.s.shutdown(socket.SHUT_WR)
-        self.s.close()
+        self.command_s.shutdown(socket.SHUT_WR)
+        self.command_s.close()
+
+    def videoStream(self):
+        print(sendCommand("stream on"))
+        self.video_s.connect(("192.168.2.1", 40921))
+
+        video = cv2.VideoCapture()
+
+        self.video_s.shutdown(socket.SHUT_WR)
+        self.video_s.close()
